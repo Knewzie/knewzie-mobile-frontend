285ecddda48ebbc2c0106079284f74a72ffc6a36
晟章 <shengzhang.wsz@antgroup.com>
2021-08-26 22:08:24 +0800

    add editor

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-    configureWebpack: {
-        plugins: [
-        ]
+    pages: {
+        index: {
+            entry: "src/main.js",
+            template: "public/index.html"
+        },
+        editor: {
+            entry: "src/editor/main.js",
+            template: "public/editor.html",
+            title: "编辑器"
+        }
     }
 }
