cb41b215eb3eebca9a128654417ad937e22a7704
linqing <7588281+Can_stop_biting@user.noreply.gitee.com>
2022-12-21 16:42:12 +0800

    动态图片大小

--- src/components/WaveMultiImage.vue
+++ src/components/WaveMultiImage.vue
@@ -1,48 +1,44 @@
 <template>
-
-    <div class="image-wrapper">
-      <div  v-for="(item,index) in dataList" :key="index">
-         <img :src="item" v-on:click="openApp" />
-      </div>
-      <!-- 图片超过4张 -->
-      <div v-if="mediaList.length > 4" class="image-item-more-wrapper"> 
-        <img :src="mediaList[3]"   />
-        <div class="image-item-more" >+3</div>
-      </div>
+  <div class="image-wrapper">
+    <div v-for="(item, index) in dataList" :key="index">
+      <img :src="item" v-on:click="openApp" />
     </div>
-
+    <!-- 图片超过4张 -->
+    <div v-if="mediaList.length > 4" class="image-item-more-wrapper">
+      <img :src="mediaList[3]"   />
+      <div class="image-item-more">+3</div>
+    </div>
+  </div>
 </template>
 <script>
-
 export default {
-  name: 'WaveMultiImage',
-  components: {
-  },
+  name: "WaveMultiImage",
+  components: {},
   props: {
     mediaList: {
       type: Array,
-      default: () => {return [];}
+      default: () => {
+        return [];
+      },
     },
   },
   data() {
-      return {
-        
-      }
+    return {};
   },
   computed: {
-     dataList(){
+    dataList() {
       if (this.mediaList.length > 4) {
-        return this.mediaList.slice(0,3);
+        return this.mediaList.slice(0, 3);
       }
       return this.mediaList;
-     }
+    },
   },
   methods: {
-      openApp () {
-        this.$emit('onClickCall');
-      }
-  }
-}
+    openApp() {
+      this.$emit("onClickCall");
+    },
+  },
+};
 </script>
 
 
@@ -51,35 +47,44 @@ export default {
 .image-wrapper {
   /* text-align: center; */
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-between;
 }
 .image-wrapper img {
-  width: 77px;
-  height: 77px;
+  width: 21vw;
+  height: 21vw;
   border-radius: 15px;
   object-fit: cover;
 }
-.image-wrapper div {
+/* .image-wrapper div {
   margin-left: 5px;
-}
+} */
 
 .image-item-more-wrapper {
   background: rgba(23, 23, 23);
   /* opacity:0.8; */
   border-radius: 15px;
-  width: 77px;
-  height: 77px;
+  width: 21vw;
+  height: 21vw;
   position: relative;
 }
 .image-item-more-wrapper img {
-  opacity:0.3; 
+  width: 21vw;
+  height: 21vw;
+  opacity: 0.3;
   position: absolute;
 }
 .image-item-more {
-  text-align: center;
-  top: 28px;
-  position: absolute;
-  left: 24px;
+  /* text-align: center; */
+  width: 21vw;
+  height: 21vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -10vw;
+  margin-left: -10vw; */
   color: white;
 }
 </style>
