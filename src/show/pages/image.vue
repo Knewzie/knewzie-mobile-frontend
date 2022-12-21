e21eb5e79cf19076d7950496a3b91ca88aa0bd2b
ryanzl <killerggf@126.com>
2022-12-21 15:56:47 +0800

    调试Android端分享

--- src/show/pages/image.vue
+++ src/show/pages/image.vue
@@ -290,8 +290,8 @@ export default {
       }
     },
     launchApp() {},
-    launchError() {
-      // alert(err.detail.errMsg);
+    launchError(err) {
+      alert(err.detail.errMsg);
       this.oia();
     },
     download() {
@@ -371,7 +371,7 @@ export default {
       .then((response) => {
         const { data: sign } = response.data;
         wx.config({
-          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
           appId: appId, // 必填，公众号的唯一标识
           timestamp: timestamp, // 必填，生成签名的时间戳
           nonceStr: nonceStr, // 必填，生成签名的随机串
