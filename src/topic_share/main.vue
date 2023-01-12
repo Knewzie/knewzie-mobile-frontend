bfb0f76e9cc3cadbb42af5806c0518b2f78a564a
ryanzl <killerggf@126.com>
2023-01-12 17:16:02 +0800

    测试

--- src/topic_share/main.vue
+++ src/topic_share/main.vue
@@ -287,7 +287,7 @@ export default {
         var filterContent = htmlFilter.filter(this.article.content);
         // alert(sign);
         wx.config({
-          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
           appId: appId, // 必填，公众号的唯一标识
           timestamp: timestamp, // 必填，生成签名的时间戳
           nonceStr: nonceStr, // 必填，生成签名的随机串
@@ -369,7 +369,7 @@ export default {
       if (/MicroMessenger/i.test(window.navigator.userAgent)) {
         alert("请在浏览器里打开");
       } else {
-        window.location.assign(`zhixin:///topic/${id}`);
+        window.location.assign(`waving:///topic/${id}`);
       }
     },
     launchApp() {},
