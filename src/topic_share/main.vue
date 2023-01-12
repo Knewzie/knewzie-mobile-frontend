e3633bc25184703f5a2c4df289df1acab67e8ec3
ryanzl <killerggf@126.com>
2023-01-12 15:32:13 +0800

    测试

--- src/topic_share/main.vue
+++ src/topic_share/main.vue
@@ -287,7 +287,7 @@ export default {
         var filterContent = htmlFilter.filter(this.article.content);
         // alert(sign);
         wx.config({
-          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
           appId: appId, // 必填，公众号的唯一标识
           timestamp: timestamp, // 必填，生成签名的时间戳
           nonceStr: nonceStr, // 必填，生成签名的随机串
@@ -374,7 +374,7 @@ export default {
     },
     launchApp() {},
     launchError(err) {
-      alert(err.detail.errMsg);
+      alert(JSON.stringify(err.detail));
       this.oia();
     },
     like() {
